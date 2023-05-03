2d7d443ab258baeebe9198bf867fca3a4dc0a31c
Dominick Martin <dominickbmartin@gmail.com>
2023-05-03 02:19:59 +0000

    Lab31 Upload

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# drf-api
-401 Python Lab: Class 31 
+
+# 401 Advanced Python LAB - Class 31
+
+## Author: Dominck Martin
+
+## Project: drf-api
+
+## Project: Django REST Framework and Docker
+
+- Rebuild a custom version of Things API demo project from scratch.
+
+- Replace things_project and Thing with your own application and model.
+
+- Your model must have at least as many fields as demo’s model.
+
+- Your model must have one field that is a foreign key to user.
+
+<!-- 
+### N/A [Back-end server] ()
+### N/A [Front-end application] () -->
+
+## Links and Resources
+N/A 
+
+##  Setup
+per requirements.txt
+
+# How to initialize/run your application (where applicable)
+
+From root folder, run:
+
+python manage.py runserver
+
+<!-- ## How to use your library (where applicable) -->
+
+## Tests
+
+From root folder, run:
+
+python manage.py test
+
+
+
+
--- drummer_app/tests.py
+++ drummer_app/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Drummer
+
+
+class Drummer_appTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user1 = get_user_model().objects.create_user(username='user1', email='user1@gmail.com', password='1234')
+        user1.save()
+
+        test_drummer = Drummer.objects.create(
+            name = 'Ringo Starr',
+            genre = 'Rock',
+            drum_brand = 'Ludwig',
+        )
+        test_drummer.save()
+
+
+    # name = models.CharField(max_length=100)
+    # band = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    # drum_brand = models.CharField(max_length=100)
+    # genre = models.CharField(max_length=100)
+    # active = models.BooleanField(default=False)   
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+
+    def test_drummer_content(self):
+        drummer = drummer.objects.get(id=1)
+        actual_name = str(drummer.name)
+        actual_drum_brand = str(drummer.drum_brand)
+        actual_genre = str(drummer.genre)
+        self.assertEqual(actual_name, 'Ringo Star')
+        self.assertEqual(actual_drum_brand, 'Ludwig')
+        self.assertEqual(actual_genre, 'Rock')
 
 # Create your tests here.
